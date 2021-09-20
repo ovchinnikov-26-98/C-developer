6f79e7b11ac6921e158d22f261e3d9a4cfcd586d
ovchinnikov-26-98 <ovchinnikov.26.98@mail.ru>
2021-09-21 00:28:49 +0300

    Приложение разбиения числа на группы неделимости.
    Приложение разбиения числа на группы неделимости.

---
+++ Split_Number/Lesson6/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(File.ReadAllText("inNumber.txt"));
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Введите вариант действия:" +
+                "\n1 - рассчитать только количесвто групп и вывести на экран" +
+                "\n2 - распределить числа и записать их в файл");
+            string path = null;
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    int count = NumberOfGroups(N);
+                    Console.WriteLine($"Количество групп: {count}");
+                    break;
+                case "2":
+                    Console.WriteLine("Укадите путь и имя файла для записи. Иначе файл создаться автоматически");
+                    path = Console.ReadLine();
+                    WritingOfNumbers(path, N);
+                    Console.WriteLine("Поместиить файл в архив: \n1 - да \n2 -нет");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            CreatingAnArchive(/*path*/);
+                            TimeSpan time = DateTime.Now.Subtract(date);
+                            Console.WriteLine($"Время выполнения: {time.TotalMilliseconds}");
+                            break;
+                        case "2":
+                            break;
+                    }
+                    break;
+            }
+            
+
+            
+            
+            
+        }
+
+        static void WritingOfNumbers(string path, int N)
+        {
+            if (!File.Exists(path))
+            {
+                path = Directory.GetCurrentDirectory() + "\\outNumber.txt";
+            }
+
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                int c = NumberOfGroups(N);
+                int k;
+                sw.Write($"Группа 1: 1");
+                sw.WriteLine();
+                for (int i = 2; i <= c; i++)
+                {
+                    sw.Write($"Группа {i}:");
+                    k = (int)Math.Pow(2, i - 1);
+                    for (int j = k; j < k * 2 && j <= N; j++) sw.Write(j + " ");
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        static int NumberOfGroups(int N)
+        {
+            int count = 1;
+            while (N > 1)
+            {
+                N = N / 2;
+                count++;
+
+            }
+            return count;
+        }
+
+        static void CreatingAnArchive (/*string path*/)
+        {
+            string source = "outNumber.txt";
+            string compressed = "outNumber.zip";
+            using (FileStream ss = new FileStream(source, FileMode.OpenOrCreate))
+            {
+                using (FileStream ts = File.Create(compressed))   
+                {
+                    
+                    using (GZipStream cs = new GZipStream(ts, CompressionMode.Compress))
+                    {
+                        ss.CopyTo(cs); 
+                        Console.WriteLine("Сжатие файла {0} завершено. Было: {1}  стало: {2}.",
+                                          source,
+                                          ss.Length,
+                                          ts.Length);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Split_Number/Split_Number/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+
+namespace Split_Number
+{
+    class Program
+    {
+        /// <summary>
+        /// Функция разбиения числа на все возможные множители, и сортирующая их в различные группы неделимости.
+        /// </summary>
+        /// <param name="N">Число,разбиваемое на множители.</param>
+        /// <returns></returns>
+        static string Substracting_Number(int N)
+        {
+            string Output_Strings = "";
+
+            List<int> Nums_All = new List<int>();                                                                           //Динамический массив начальных чисел.
+            List<int> Nums_Local = new List<int>();                                                                         //Динамический массив неделимых чисел, составленных из исходного массива.
+
+            for (int i = 1; i < N; i++) { Nums_All.Add(i); }
+            int group_number = 1;                                                                                           //Начальное значение номера групп.
+            int Number_Key;                                                                                                 //Условие для проверки неделимости.
+            while (Nums_All.Count != 0)
+            {                                                                                   //Цикл, выполняющийся, пока кол-во элементов исходного массива не станет 0.
+                Nums_Local.Add(Nums_All[0]);
+                Nums_All.RemoveAt(0);
+                for (int i = 0; i < Nums_All.Count; i++)                                                                    //Перебор начального массива.
+                {
+                    Number_Key = 0;
+                    for (int j = 0; j < Nums_Local.Count; j++)                                                              //Перебор нового массива.
+                    {
+                        if (Nums_All[i] % Nums_Local[j] != 0) { Number_Key++; }                                              //Условие неделимости.
+                    }
+                    if (Number_Key == Nums_Local.Count)                                                                     //Заполнение нового массива,удаление элементов старого массива, если условие неделимости выполнено.
+                    {
+                        Nums_Local.Add(Nums_All[i]);
+                        Nums_All.RemoveAt(i);
+                    }
+                }
+                Output_Strings += "Группа № " + Convert.ToString(group_number) + ":";                                       //Запись в выходную строку полученного массива.
+                for (int i = 0; i < Nums_Local.Count; i++) { Output_Strings += " " + Convert.ToString(Nums_Local[i]); }
+                Output_Strings += "\r\n";
+
+                Nums_Local.Clear();                                                                                         //Очистка массива.
+                group_number++;
+
+            }
+            //Console.WriteLine(Output_Strings);
+            return Output_Strings;
+        }
+
+        /// <summary>
+        /// Точка входа в приложение.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string Files_Path = @"C:\Users\Kirill\Desktop";                                                     //Адрес хранения файлов.
+            string Input_File_Name = "Input File.txt";                                                          //Имя входного файла.
+            int N = Convert.ToInt32(File.ReadAllText(@$"{Files_Path}\{Input_File_Name}"));                      //Добавить исключение-проверку на наличие файла в папке.
+
+            if (N > 1_000_000_000) { Console.WriteLine("Too long number!"); }
+            else
+            {
+                string OutPut_File_Name = "OutPut File.txt";                                                    //Имя выходного файла.
+                char Answer;
+                bool AnswerKey = true;
+
+                DateTime start = new DateTime();                                                                //Время(дата) начала работы приложения.
+                string strings = Substracting_Number(N);                                                        //Получение результирующей строки групп неделимости.
+                DateTime finish = new DateTime();                                                               //Время(дата) конца работы приложения.
+                TimeSpan time = finish.Subtract(start);                                                         //Время работы приложения.
+                Console.WriteLine($"time = {time.Milliseconds}");
+
+                File.WriteAllText(@$"{Files_Path}\{OutPut_File_Name}", strings);
+                Console.Write("Add results to the archive?(Y/N): ");
+                Answer = Convert.ToChar(Console.ReadLine()[0]);
+                while (AnswerKey == true)
+                {
+                    if ((Answer == 'N') || (Answer == 'Y')) { AnswerKey = false; }
+                    else
+                    {
+                        Console.WriteLine("Incorrect form!");
+                        Console.Write("Add results to the archive?(Y/N): ");
+                        Answer = Convert.ToChar(Console.ReadLine()[0]);
+                    }
+
+                }
+                //Архивация результатов.
+                if (Answer == 'Y')
+                {
+                    string Output_File_Archive_Name = "Output_File_Archive.zip";
+                    using (FileStream OutPut_Stream = new FileStream(@$"{Files_Path}\{OutPut_File_Name}", FileMode.OpenOrCreate))                      // Поток данных ,которые необходимо записать/архивировать.
+                    {
+                        using (FileStream New_Archive_Stream = File.Create(@$"{Files_Path}\{Output_File_Archive_Name}.zip"))                               // Поток - создание нового файла - архива.
+                        {
+                            using (GZipStream Archivating_Stream = new GZipStream(New_Archive_Stream, CompressionMode.Compress))    // Поток архивации данных потока создания нового файла(архива). 
+                            {
+                                OutPut_Stream.CopyTo(Archivating_Stream);                                                           // Копирование данных из входных данных(потока) в выходной.
+                                Console.WriteLine("Archivating is over! Old Memory:{0} , New Memory:{1}.", OutPut_Stream.Length, New_Archive_Stream.Length);
+                            }
+                        }
+                    }
+                    Console.WriteLine("Enter to exit.");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
